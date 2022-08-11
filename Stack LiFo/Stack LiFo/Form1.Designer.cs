82eb370f2eaca2dfce330a2b8bb9f9ce122a0949
Philip Njoroge <njorophil@gmail.com>
2022-08-11 16:07:39 +0300

    FINAL

---
+++ Stack LiFo/Stack LiFo/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Stack_LiFo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panelID1 = new System.Windows.Forms.Panel();
+            this.panelID2 = new System.Windows.Forms.Panel();
+            this.panelID3 = new System.Windows.Forms.Panel();
+            this.panelID4 = new System.Windows.Forms.Panel();
+            this.btnRandomStack = new System.Windows.Forms.Button();
+            this.btnRemoveStackLiFo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Silver;
+            this.groupBox1.Location = new System.Drawing.Point(280, 96);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(214, 264);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Stack Imagery";
+            // 
+            // panelID1
+            // 
+            this.panelID1.BackColor = System.Drawing.Color.White;
+            this.panelID1.Location = new System.Drawing.Point(542, 127);
+            this.panelID1.Name = "panelID1";
+            this.panelID1.Size = new System.Drawing.Size(149, 41);
+            this.panelID1.TabIndex = 3;
+            // 
+            // panelID2
+            // 
+            this.panelID2.BackColor = System.Drawing.Color.White;
+            this.panelID2.Location = new System.Drawing.Point(543, 185);
+            this.panelID2.Name = "panelID2";
+            this.panelID2.Size = new System.Drawing.Size(149, 41);
+            this.panelID2.TabIndex = 2;
+            // 
+            // panelID3
+            // 
+            this.panelID3.BackColor = System.Drawing.Color.White;
+            this.panelID3.Location = new System.Drawing.Point(543, 245);
+            this.panelID3.Name = "panelID3";
+            this.panelID3.Size = new System.Drawing.Size(149, 41);
+            this.panelID3.TabIndex = 1;
+            // 
+            // panelID4
+            // 
+            this.panelID4.BackColor = System.Drawing.Color.White;
+            this.panelID4.Location = new System.Drawing.Point(543, 302);
+            this.panelID4.Name = "panelID4";
+            this.panelID4.Size = new System.Drawing.Size(149, 41);
+            this.panelID4.TabIndex = 0;
+            // 
+            // btnRandomStack
+            // 
+            this.btnRandomStack.Location = new System.Drawing.Point(12, 51);
+            this.btnRandomStack.Name = "btnRandomStack";
+            this.btnRandomStack.Size = new System.Drawing.Size(211, 39);
+            this.btnRandomStack.TabIndex = 1;
+            this.btnRandomStack.Text = "Stack Random Colors";
+            this.btnRandomStack.UseVisualStyleBackColor = true;
+            this.btnRandomStack.Click += new System.EventHandler(this.btnRandomStack_Click);
+            // 
+            // btnRemoveStackLiFo
+            // 
+            this.btnRemoveStackLiFo.Location = new System.Drawing.Point(577, 51);
+            this.btnRemoveStackLiFo.Name = "btnRemoveStackLiFo";
+            this.btnRemoveStackLiFo.Size = new System.Drawing.Size(211, 39);
+            this.btnRemoveStackLiFo.TabIndex = 2;
+            this.btnRemoveStackLiFo.Text = "Remove Stack LiFo";
+            this.btnRemoveStackLiFo.UseVisualStyleBackColor = true;
+            this.btnRemoveStackLiFo.Click += new System.EventHandler(this.btnRemoveStackLiFo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelID1);
+            this.Controls.Add(this.btnRemoveStackLiFo);
+            this.Controls.Add(this.panelID2);
+            this.Controls.Add(this.btnRandomStack);
+            this.Controls.Add(this.panelID3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panelID4);
+            this.Name = "Form1";
+            this.Text = "Stack - Last In First Out";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Panel panelID1;
+        private Panel panelID2;
+        private Panel panelID3;
+        private Panel panelID4;
+        private Button btnRandomStack;
+        private Button btnRemoveStackLiFo;
+    }
+}
---
+++ Stack LiFo/Stack LiFo/Form1.cs
@@ -0,0 +1,172 @@
+using System.Collections;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Stack_LiFo
+{
+    public partial class Form1 : Form
+    {
+
+        Stack<Panel> _colorStack = new Stack<Panel>();
+        Panel p = new Panel();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Panel RandomColourGenerator(int n)
+        {
+            //Random rnd = new Random();
+            //Color randomColor = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+
+            //return randomColor;
+
+            
+
+            Random randomGen = new Random();
+            KnownColor[] names = (KnownColor[])Enum.GetValues(typeof(KnownColor));
+            KnownColor randomColorName = names[randomGen.Next(names.Length)];
+            Color randomColor = Color.FromKnownColor(randomColorName);
+
+            Panel panel = new Panel();
+            panel.Name = $"panelID{n}";
+            panel.BackColor = randomColor;
+
+            return panel;
+        }
+
+        private void btnRandomStack_Click(object sender, EventArgs e)
+        {
+            _colorStack.Push(RandomColourGenerator(1));
+            _colorStack.Push(RandomColourGenerator(2));
+            _colorStack.Push(RandomColourGenerator(3));
+            _colorStack.Push(RandomColourGenerator(4));
+
+
+            //for (int i = 0; i < _colorStack.Count; i++)
+            //{
+            //    p.Name = "panelID" + (i);
+            //    p.BackColor = RandomColourGenerator();
+            //    p.Dock = DockStyle.Top;
+            //}
+
+
+            int PanelID = 1;
+            /*foreach (object obj in _colorStack)
+            {
+                p.Name = "panelID" + (PanelID);
+                //setBackground((Color)obj, p.Name);
+                p.BackColor = (Color)obj; //  (Color)obj;
+                //p.Dock = DockStyle.Top;
+                PanelID++;
+                await Task.Delay(500);
+            }*/
+            setBackground(_colorStack);
+        }
+
+        private void btnRemoveStackLiFo_Click(object sender, EventArgs e)
+        {
+            //_colorStack.Pop();
+
+            //for (int i = 0; i < _colorStack.Count; i++)
+            //{
+            //    p.Name = "panelID" + (i);
+            //    p.BackColor = Color.White;
+            //    p.Dock = DockStyle.Top;
+            //}
+
+            setBackground(_colorStack, true);
+        }
+
+        public async void setBackground(Stack<Panel> ColorStack, bool IsRemoved = false)
+        {
+            List<Control> panelControl = new List<Control>();
+            foreach (Control control in this.Controls)
+            {
+                if (control.GetType() == typeof(System.Windows.Forms.Panel))
+                {
+                    panelControl.Add(control);
+                    if (!IsRemoved)
+                    {
+                        foreach (var obj in ColorStack)
+                        {
+                            //control.Name = obj;
+                            if (control.Name.Equals(obj.Name))
+                            {
+                                control.BackColor = obj.BackColor;
+                            }
+                            await Task.Delay(500);
+                        }
+                    }
+                    /*else
+                    {
+                        //var element = ColorStack.Reverse().ElementAt(ColorStack.Count - 1);
+                        foreach (var obj in ColorStack.Reverse())
+                        {
+                            if (control.Name.Equals(obj.Name))
+                            {
+                                control.BackColor = Color.White;
+                            }
+                            await Task.Delay(500);
+                        }
+                    }*/
+                }
+            }
+
+            if (IsRemoved)
+            {
+                foreach (Control control in ReversePanel(panelControl))
+                {
+                    if (control.GetType() == typeof(System.Windows.Forms.Panel))
+                    {
+                        //var panel = ColorStack.Peek();
+
+                        ReversePanelColor(ColorStack);
+
+                        foreach (var obj in ColorStack)
+                        {
+                            if (control.Name.Equals(obj.Name))
+                            {
+                                control.BackColor = Color.White;
+                            }
+                            await Task.Delay(500);
+                        }
+                    }
+                }
+            }
+        }
+
+        public List<Control> ReversePanel(List<Control> panelControls)
+        {
+            panelControls.Reverse();
+            return panelControls;
+        }
+
+        public void ReversePanelColor(Stack<Panel> stack)
+        {
+            if (stack.Count != 0)
+            {
+                var panel = stack.Pop(); // Store the last element panel object in memory 
+                ReversePanelColor(stack); // New stack collection after removing the last element
+                RearrangePanelColor(stack, panel); // New stack collection after removing the last element and maintaining the last element reference
+            }
+        }
+
+        public void RearrangePanelColor(Stack<Panel> stack, Panel panel)
+        {
+            if (stack.Count == 0)
+            {
+                stack.Push(panel);
+            }
+            else
+            {
+                var panelTemp = stack.Pop(); // Store reference of the last element after the reverse function has popped the stack
+                RearrangePanelColor(stack, panel); // Reference after deleting the panel and accessing the panel object for rearrangment
+                stack.Push(panelTemp); // store the panel object for rearrangement
+            }
+        }
+
+
+    }
+}
